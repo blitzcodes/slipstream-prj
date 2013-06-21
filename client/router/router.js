ed6136a57599e9e0f80a84dea67352c067d0b3c1
Sean 'Blitz' Homer <shomer@rcdesign.com>
2013-06-21 11:19:11 -0400

    - Tweaks to reflect mrt package changes

--- client/router/router.js
+++ client/router/router.js
@@ -1,10 +1,1 @@
 Meteor.Router.add('/', {to : 'root', as : 'home'});
-
-Meteor.Router.filters({
-	'clearErrors' : function (page) {
-		clearErrors();
-		return page;
-	}
-});
-
-Meteor.Router.filter('clearErrors');
